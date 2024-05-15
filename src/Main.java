8006e835302a3b68f22a36e35f55f68a3f6319cd
DouyonDev <abdoulayedouyon.ad@gmail.com>
2024-05-15 19:51:06 +0000

    connexion du passager a la base de donnee avec hachage du mot de passe

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 import models.*;
-
+import java.sql.SQLException;
+import java.util.Scanner;
+import org.mindrot.jbcrypt.BCrypt;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         Connexion.seConecter();
-        Pays.ajouterPays(Connexion.con, scanner);
-        System.out.println("Hello!");
+
+        // Connexion du passager à la base de données
+        Scanner entree = new Scanner(System.in);
+        System.out.println("Pour vous connecter Veuillez donner votre E-mail : ");
+        String email = entree.nextLine();
+        System.out.println("Donnez votre mot de passe : ");
+        String motDePasse = entree.nextLine();
+        String HashermotDePasseFourni = BCrypt.hashpw(motDePasse, BCrypt.gensalt());
+
+        Passager passager = new Passager(null,null,email,null,null,HashermotDePasseFourni);
+        passager.seConnecter(email, motDePasse);
+
+
+
     }
 }
