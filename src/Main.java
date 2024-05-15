4ff7dce2a8701911e05f5a8ca52335885feaa617
DouyonDev <abdoulayedouyon.ad@gmail.com>
2024-05-15 20:01:33 +0000

    inscription et connexion du passager a la base de donnee avec hachage du mot de passe

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Connexion.seConecter();
 
+
+
+    }
+
+    private void InscriptionPassager() throws SQLException {
+        // Workflow pour l'inscription
+        Scanner entree = new Scanner(System.in);
+        System.out.println("Veuillez entrer vos informations :\n");
+        //Nom Passager
+        System.out.println("Votre nom de Famille :");
+        String nom = entree.nextLine();
+        //Prenom Passager
+        System.out.println("Votre Prenom :");
+        String prenom = entree.nextLine();
+        //Email
+        System.out.println("Votre Email :");
+        String email = entree.nextLine();
+        //Numero de telephone
+        System.out.println("Votre numero de telephone :");
+        String numTelephone = entree.nextLine();
+        //Date de Naissance
+        String DateNaissance = util.Date();
+        //Mot de Passe
+        System.out.println("Donnez un mot de passe :");
+        String motDePasse = entree.nextLine();
+        String motDePasseHasher = BCrypt.hashpw(motDePasse, BCrypt.gensalt());
+
+        // Instanciation du passager
+        Passager passager = new Passager(nom, prenom, email, numTelephone, DateNaissance, motDePasseHasher);
+        passager.inscription();
+    }
+
+    private void ConnexionPassager(){
         // Connexion du passager à la base de données
         Scanner entree = new Scanner(System.in);
         System.out.println("Pour vous connecter Veuillez donner votre E-mail : ");
@@ -19,8 +52,7 @@ public class Main {
 
         Passager passager = new Passager(null,null,email,null,null,HashermotDePasseFourni);
         passager.seConnecter(email, motDePasse);
-
-
-
     }
+
+
 }
