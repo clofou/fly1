4e85711f5c4a3b5aed324383de928fc0663faedc
77madel <madoumadeltitokone77@gmail.com>
2024-05-16 09:44:18 +0000

    V1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Connexion.seConecter();
-        Pays.ajouterPays(Connexion.con, scanner);
+       // Pays.ajouterPays(Connexion.con, scanner);
+        Vol.ajouterVol(Connexion.con,scanner);
         System.out.println("Hello Fakoro!");
     }
 }
