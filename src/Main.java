5f30bae1e880d685d19ccaa78347e815e7b1c052
DouyonDev <abdoulayedouyon.ad@gmail.com>
2024-05-17 08:35:15 +0000

    inscription et connexion admin

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         Connexion.seConecter();
-        AdminAjoutCompagnie();
+        //AdminAjoutCompagnie();
     }
 
     private void InscriptionPassager() throws SQLException {
@@ -52,5 +52,50 @@ public class Main {
         passager.seConnecter(email, motDePasse);
     }
 
+    private static void ajouterUnAdministrateur() throws SQLException {
+        //Ajout d'un administrateur
 
+        Scanner entree = new Scanner(System.in);
+        System.out.println("Veuillez entrer les informations du nouveau administrateur :\n");
+        //Nom Admin
+        System.out.println("Votre nom de Famille :");
+        String nom = entree.nextLine();
+        //Prenom Admin
+        System.out.println("Votre Prenom :");
+        String prenom = entree.nextLine();
+        //Email
+        System.out.println("Votre Email :");
+        String email = entree.nextLine();
+        //Numero de telephone
+        System.out.println("Votre numero de telephone :");
+        String numTelephone = entree.nextLine();
+        //Date de Naissance
+        String DateNaissance = util.Date();
+        //Mot de Passe
+        System.out.println("Donnez un mot de passe :");
+        String motDePasse = entree.nextLine();
+        String motDePasseHasher = BCrypt.hashpw(motDePasse, BCrypt.gensalt());
+
+        // Instanciation de l'admin
+        Admin admin = new Admin(nom, prenom, email, numTelephone, DateNaissance, motDePasseHasher);
+        //Ajout dans la base de données
+        admin.ajoutAdmin();
+    }
+
+    private static boolean ConnexionAdmin(){
+        // Connexion de l'admin à la base de données
+        Scanner entree = new Scanner(System.in);
+        System.out.println("Pour vous connecter en tant que Admin, veuillez donner votre E-mail : ");
+        String email = entree.nextLine();
+        System.out.println("Donnez votre mot de passe Admin : ");
+        String motDePasse = entree.nextLine();
+        String HashermotDePasseFourni = BCrypt.hashpw(motDePasse, BCrypt.gensalt());
+
+        Admin admin = new Admin(null,null,email,null,null,HashermotDePasseFourni);
+        //admin.seConnecter(email, motDePasse);
+        if (admin.seConnecter(email, motDePasse)){
+            return true;
+        }else return false;
+
+    }
 }
