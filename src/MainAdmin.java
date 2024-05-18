e9bb9ea3040d518f376bda5349dfbdcdcf345053
clofou <fakoro88@gmail.com>
2024-05-18 12:14:04 +0000

    Creation de l'interface Admin

---
+++ src/MainAdmin.java
@@ -0,0 +1,10 @@
+import models.Connexion;
+
+import java.util.Scanner;
+
+public class MainAdmin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Connexion.seConecter();
+    }
+}
