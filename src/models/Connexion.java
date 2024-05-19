49b26634cca8dc95056c54475e2a889d11706c91
clofou <fakoro88@gmail.com>
2024-05-19 23:11:17 +0000

    Ok

--- src/models/Connexion.java
+++ src/models/Connexion.java
@@ -11,9 +11,9 @@ public class Connexion {
 		try {
 		
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url="jdbc:mysql://localhost:8889/gestiondevolgroupe1";
+			String url="jdbc:mysql://localhost:3306/gestiondevolgroupe1";
 			String user="root";
-			String pass="root";
+			String pass="";
 			con=DriverManager.getConnection(url,user,pass);
 			System.out.println("Connection reussit ");
 		} catch (ClassNotFoundException e) {
